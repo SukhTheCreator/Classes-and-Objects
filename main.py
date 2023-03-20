31b30fc834ce5f2cb000a1080b1cf47d467f954a
Sukhmaan Singh <128427871+SukhTheCreator@users.noreply.github.com>
2023-03-20 21:47:01 +0000

    function

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 class House:
   #constructor method/function are called methods
   #variables in classes are attributes
-  def __init__(self, area, windows, width, height, color, floors, occupied):
-    self.area = area
+  def __init__(self, windows, width, height, color, floors, occupied):
+    # self.area = area
     self.windows = windows
     self.width = width
     self.height = height
@@ -11,6 +11,14 @@ class House:
     self.floors = floors
     self.occupied = occupied
 
-  def area(width, height):
+  def area(self, width, height):
     return width * height
 
+redHouse = House (3, 15, 20, "red", 2, True)
+print(redHouse.area(34, 25))
+
+blueHouse = House (5, 36, 30, "blue", 4, True)
+print(blueHouse.area(40, 32))
+
+greyHouse = House (4, 26, 40, "grey", 2, True)
+print(greyHouse.area(25, 30))
